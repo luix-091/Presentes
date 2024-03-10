4d9fa3fb660ca51b844a6299621ce1dd2644c7e8
luix-091 <luiz.filipe.ctt@gmail.com>
2024-03-10 17:55:45 -0300

    v2.0

--- Presentes/settings.py
+++ Presentes/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Presentes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'vnbgSxMRFgthJDsNZByLQwVKHveddoeK',
+        'HOST':'monorail.proxy.rlwy.net',
+        'PORT':'23161'
     }
 }
 
@@ -131,6 +135,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
