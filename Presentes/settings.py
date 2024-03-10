ffb776bdf2da933c8a79545b95da894def1927eb
luix-091 <luiz.filipe.ctt@gmail.com>
2024-03-10 15:45:18 -0300

    v1.2

--- Presentes/settings.py
+++ Presentes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xj=xlfrw50^1+-4e68u@g=7o-7d95#e+u$$n8=nx5u7--pi4uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 LOGIN_REDIRECT_URL = 'home'
 
